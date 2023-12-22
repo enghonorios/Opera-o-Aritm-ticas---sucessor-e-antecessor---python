d2664a26640265994e54db5a92a83ed972304f26
enghonorios <154471089+enghonorios@users.noreply.github.com>
2023-12-22 18:29:32 -0300

    Operação Aritméticas - sucessor e antecessor - Python

---
+++ Operação Aritméticas - sucessor e antecessor - python.py
@@ -0,0 +1,14 @@
+# recebe um número inteiro do usuário
+
+n1= int(input('Digite um número inteiro: '))
+resultado= n1
+
+# calcula o sucessor e o antecessor
+
+sucessor= resultado + 1
+antecessor= resultado -1
+
+# mostra os resultados na tela
+
+print(f'O número digitado é {resultado}.')
+print(f'Seu sucessor é {sucessor} seu antecessor é {antecessor}.')
